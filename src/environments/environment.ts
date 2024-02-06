368b196efd4c8891fd85aaab60516e3351dfaa9e
Gianfrano Giordano <gianfranogiordano@gmail.com>
2024-02-06 12:02:38 -0500

    pixel y api fb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
   cloudFront: 'https://d3cj1y2eynyat2.cloudfront.net',
   clickstoreId: '65148d449f932e07bf93e0b0',
   clicstoreDomain: 'https://shop.alpelocol.com',
-  pixel: '1411759042749299',
-  apiFB: 'EAAJVxS9CCwMBO3Q6JX9ZAK6oG1mRuogimAon23JJf9TEnBybhrHt0whHKaUQq59T2GDxx27yut7n32zMy3Ao2XK2dLmNZC1j8PaSbbLobV9auIYaRsKssaqZCOamx673pRDud82mYq8ZChwoZBZCjUIGQBG4EoW6JzmMdEOkayV9TshqSc32Fuw46j0WRCfn0oUAZDZD'
+  pixel: '1608434613020918',
+  apiFB: 'EAAQriufrRAABOZBaC3TkyzFXYqGbDva8Lx6I0SOI8nZCmKyvpjcRkYLQBZBAyJ9w8bzQY5xxZCYMR1jRoxnHGxlDmrZB2ZCd0nwynR7RDqcs0h1ttkot7FfPEhalAQL65a0qFTSvPZBbviRIkR86C9ZA7KgxRJYxMBrYakL4SE8h6Q1HxwZAGOVhUHd7lioFmi5zZAggZDZD'
 };
 
 /*
